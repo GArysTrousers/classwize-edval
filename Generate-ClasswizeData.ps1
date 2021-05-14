3d7f9c81668facaa5c7c37558a808bdc335368cc
GArysTrousers <bjonmere@gmail.com>
2021-05-14 14:54:44 +1000

    init

---
+++ Generate-ClasswizeData.ps1
@@ -0,0 +1,46 @@
+#import csv and convert to collection object
+$csv = {Import-Csv -Path "All students.csv" -Header StudentCode, Surname, Firstname, Year, FloatsLetter, FloatsNumber, CourseCode, ClassId, ClassYear, TeacherCode, TeacherName, Subject}.Invoke()
+#remove header row
+if ($csv[0].StudentCode -notmatch '[A-Za-z]{3}[0-9]{4}') { $csv.RemoveAt(0) }
+
+#Students
+Write-Host "Adding Students..." -NoNewline
+$list = @()
+$csv | ForEach-Object {
+  $list += [PSCUSTOMOBJECT]@{
+    username  = $_.StudentCode
+    class     = $_.CourseCode + "_" + $_.ClassId
+    isTeacher = "false"
+  }
+}
+Write-Host "Done."
+
+#Teachers
+Write-Host "Adding Teachers..." -NoNewline
+$sigs = @()
+$csv | ForEach-Object {
+  #ignore teacherless classes
+  if ($_.TeacherCode -ne "" -and $_.CourseCode -ne "") {
+    try {
+      #tests if user exists
+      Get-ADUser -Identity $_.TeacherCode | Out-Null
+      #if item is not in List already
+      $sig = $_.TeacherCode + ',' + $_.CourseCode + "_" + $_.ClassId + ",true"
+      if (!$sigs.Contains($sig)) {
+        $sigs += $sig
+        $list += [PSCUSTOMOBJECT]@{
+          username  = $_.TeacherCode
+          class     = $_.CourseCode + "_" + $_.ClassId
+          isTeacher = "true"
+        }
+      }
+    }
+    catch {
+      Write-Error $_
+    }
+  }
+}
+Write-Host "Done."
+
+$list | Export-Csv -Path "./Classwize.csv" -NoTypeInformation -Delimiter ","
+Write-Host "File Exported: Classwize.csv"
---
+++ README.md
@@ -0,0 +1,28 @@
+# What is this?
+This is a powershell script to generate a csv of classes for import into Classwize.
+
+It will assign both teachers and students to their classrooms.
+
+You will need the support team at Linewize to drop all your current classes before importing the new ones.
+
+# How to use
+## Exporting EdVal Data
+1. Open EdVal Staff
+2. Classes -> Export class list
+    - Rot 1+2 = Semester 1
+    - Rot 3+4 = Semester 2
+3. Save the export into the script folder and name it "All students.csv"
+4. Run Generate-ClasswizeData.ps1
+6. This will write the output to "Classwize.csv"
+
+## Importing Classes
+1.	Go to Linewize Console
+2.	Configuration -> Classroom -> Classrooms
+3.	Import CSV
+4.	Select Upload Classrooms
+5.	Select "Classwize.csv"
+
+## Notes
+- Your "All students.csv" should have the following columns in this order:
+
+  Student code,Surname,First name,Year,FloatsLetter,FloatsNumber,Course code,Class id,Class year,Teacher code,Teacher name,Subject
